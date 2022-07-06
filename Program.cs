64818dd52a7101c7e91dcaeaafdfc0e0c6679e00
Julia <granovskaya.97@mail.ru>
2022-07-06 20:07:31 +0300

    Add file

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Принять на вход число A и выдать сумму чисел от 1 до A
+// int sumNumbers(int num)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.Write("Введите число: ");
+//     int A = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine($"Сумма чисел от 1 до {A}: {sumNumbers(A)}");
+// }
+
+// Вывести массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+// int[] GenerateRandomArray(int N, int start, int end)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end+1);
+//     }
+//     return RandomArray;
+// }
+// int[] GenerateArray(int N)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         Console.WriteLine($"Введите {i+1} элемент массивa");
+//         RandomArray[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return RandomArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+//  for (int i = 0; i < array.Length; i++)
+//  {
+//      Console.Write(array[i] +" ");
+//  }   
+//     Console.WriteLine();
+// }
+// Console.WriteLine("Введите количество элементов в массиве");
+// int num = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Введите первое число случайно генерируемого диапазона");
+// // int min = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
+// // int max = Convert.ToInt32(Console.ReadLine());
+
+// // int[] myArray = GenerateRandomArray(num, min, max);
+// // ShowArray(myArray);
+
+// int[] myArray = GenerateArray(num);
+// ShowArray(myArray);
