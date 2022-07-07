d88627121e71f0e6f0d5cf86017e0390da8d02fa
Julia <granovskaya.97@mail.ru>
2022-07-07 18:02:16 +0300

    Made homework

--- Program.cs
+++ Program.cs
@@ -16,12 +16,12 @@
 // }
 
 // Вывести массив из 8 элементов, заполненный нулями и единицами в случайном порядке
-// int[] GenerateRandomArray(int N, int start, int end)
+// int[] GenerateRandomArray(int N)
 // {
 //     int[] RandomArray = new int[N];
 //     for (int i = 0; i < N; i++)
 //     {
-//         RandomArray[i] = new Random().Next(start, end+1);
+//         RandomArray[i] = new Random().Next();
 //     }
 //     return RandomArray;
 // }
@@ -46,16 +46,18 @@
 // }
 // Console.WriteLine("Введите количество элементов в массиве");
 // int num = Convert.ToInt32(Console.ReadLine());
-// // Console.WriteLine("Введите первое число случайно генерируемого диапазона");
-// // int min = Convert.ToInt32(Console.ReadLine());
-// // Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
-// // int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число случайно генерируемого диапазона");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
+// int max = Convert.ToInt32(Console.ReadLine());
 
-// // int[] myArray = GenerateRandomArray(num, min, max);
-// // ShowArray(myArray);
-
-// int[] myArray = GenerateArray(num);
+// int[] myArray = GenerateRandomArray(num, min, max);
 // ShowArray(myArray);
+
+// int[] myArray = GenerateRandomArray(num);
+// ShowArray(myArray);
+
+// Домашнее задание
 
 // Цикл, который принимает на вход 2 числа (A и B) и возводит число A в натуральную степень B.
 // int NumberDegree(int number1, int number2)
@@ -72,4 +74,52 @@
 // Console.WriteLine("Введите число B: ");
 // int B = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine($"{A}, {B} -> {NumberDegree(A, B)}");
+
+// Принимает число и выдает сумму цифр в числе
+// int SumNumbers(int num)
+// {
+//     int sum = 0;
+//     while (num > 0)
+//     {
+//         sum = sum + num % 10;
+//         num = (num - num % 10) / 10;
+//     }
+//     return sum;
+// }
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{number} -> {SumNumbers(number)}");
+
+// Программа создает массив из 8 элементов и выводит их на экран
+// int [] FillArray(int N, int start, int end)
+// {
+//     int [] collection = new int[N];
+//     int index = 0;
+//     while (index < N)
+//     {
+//         collection[index] = new Random().Next(start, end + 1);
+//         index++;
+//     }
+//     return collection;
+// }
+
+// void PrintArray(int [] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write(col[position] +" ");
+//         position++;
+//     }
+// }
+// Console.WriteLine("Введите количество элементов в массиве");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное значение случайно генерируемого диапазона");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное значение случайно генерируемого диапазона");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int [] myArray = FillArray(num, min, max);
+// PrintArray(myArray);
 
